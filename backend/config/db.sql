862ae69cdff5e3053a80bae3969f1eed26d184d5
Sivaramakrishnan <sivaramakrishnan0327@gmail.com>
2024-11-28 20:01:56 +0530

    MySQL Schema Added

--- backend/config/db.sql
+++ backend/config/db.sql
@@ -12,7 +12,38 @@ CREATE TABLE admin (
 );
 
 -- Step 4: Insert the admin username and password
--- Ensure that the password is hashed for security reasons
 INSERT INTO admin (username, password)
 VALUES ('admin', 'admin');
+
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    roll_no VARCHAR(50) NOT NULL UNIQUE,
+    department VARCHAR(50) NOT NULL,
+    studying_year VARCHAR(50) NOT NULL,
+    section VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(15) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE events (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    from_time TIME NOT NULL,
+    to_time TIME NOT NULL,
+    event_date DATE NOT NULL,
+    day VARCHAR(20) NOT NULL,
+    venue VARCHAR(100) NOT NULL,
+    event_name VARCHAR(100) NOT NULL,
+    event_coordinator_name VARCHAR(100) NOT NULL,
+    event_coordinator_phone VARCHAR(15) NOT NULL,
+    student_coordinator_name VARCHAR(100) NOT NULL,
+    student_coordinator_phone VARCHAR(15) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
 
